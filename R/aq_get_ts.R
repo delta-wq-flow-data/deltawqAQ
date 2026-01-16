567a7157bb3700cdc65c5cd748951dd54112c89f
Catarina Pien <catarina.pien@gmail.com>
2026-01-16 14:28:24 -0800

    Changed disconnect part of this function

--- R/aq_get_ts.R
+++ R/aq_get_ts.R
@@ -1,10 +1,14 @@
 # Function to get a timeseries
 aq_get_ts = function(station_code, parameter, label, query_from, query_to,
                      write = FALSE, output_path = NULL) {
+
   # Function to connect to aquarius
   aq_connect(server_hostname = Sys.getenv("AQTS_SERVER"),
              username = Sys.getenv("AQTS_USERNAME"),
              password = Sys.getenv("AQTS_PASSWORD"))
+
+  # Ensure disconnect on exit
+  on.exit(aq_disconnect())
 
   # Function to obtain time series data and return or write df based on inputs
   df <- aq_process_ts(station_code, parameter, label, query_from, query_to)
@@ -19,20 +23,9 @@ aq_get_ts = function(station_code, parameter, label, query_from, query_to,
 
     write.csv(df, output_file, row.names = FALSE)
     cat("Data written to:", output_file, "\n")
-    return(invisible(df))
+    # return(invisible(df))
   }
 
   # Return normally if not writing
   return(df)
-
-  # Disconnect from Aquarius
-  cat("\nDisconnecting from AQUARIUS...\n")
-
-  # Ensure disconnection happens even if there were errors
-  tryCatch({
-    timeseries$disconnect()
-    cat("Disconnected successfully.\n")
-  }, error = function(e) {
-    cat("Warning: Error during disconnect:", conditionMessage(e), "\n")
-})
 }
