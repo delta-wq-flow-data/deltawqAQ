9f31221bb24b8dbfe3fd5b3d27ffb26b2e693eb4
Catarina Pien <catarina.pien@gmail.com>
2026-01-22 16:08:15 -0800

    Questions for discussion

---
+++ R/questions.R
@@ -0,0 +1,13 @@
+## Questions
+
+# Do we want multi param and multi station functions to output combined or single files?
+  # Or an option for either? Currently it returns a combined file but writes separate  files.
+# Currently a lot of messages/alerts... determine which ones we want to keep after testing
+# Do we want to edit to try to call less functions and use more of a static file or caching?
+# use parameter ID instead of parameter name?
+# Anything needed for roxygen for timeseriesClient? Also general edits for the client?
+# Specifications for R and package versions? - Dependencies
+# Should each function have its own file? Have read conflicting advice. Could also reorganize to put "helpers" in one file, and each
+  # other function in its own.
+# How much do we want to get into tests/testthat?
+
