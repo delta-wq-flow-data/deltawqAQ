4858e24fb8e80d5bf52b4b8e323d6afeaadc11d2
Catarina Pien <catarina.pien@gmail.com>
2026-01-23 10:12:44 -0800

    Added functions that need to be imported but aren't expressly called

---
+++ R/imports.R
@@ -0,0 +1,23 @@
+# Global imports for connections and messages
+
+#' @importFrom httr
+#'  GET
+#'  POST
+#'  DELETE
+#'  parse_url
+#'  set_config
+#'  timeout
+#'  stop_for_status
+#'  content
+#'  handle_reset
+#' @importFrom cli
+#'   cli_alert_success
+#'   cli_alert_danger
+#'   cli_alert_info
+#'   cli_alert_warning
+#'   cli_abort
+#'   cli_h2
+#'   cli_ol
+#' @importFrom jsonlite
+#'   fromJSON
+NULL
