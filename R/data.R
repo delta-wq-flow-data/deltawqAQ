b778e30a4bcbff2d068095216fba1113cbded079
Catarina Pien <catarina.pien@gmail.com>
2026-01-28 09:39:10 -0800

    Added dataset documentation

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' Aquarius Location Metadata
+#' @title Aquarius Location Metadata
+#' @description Metadata for all locations in Aquarius database
+#' @format Data frame of 9 columns x Number of Aquarius locations
+#' @source Aquarius API, as called through the 'aq_get_location_list' function
+"aq_all_locations"
