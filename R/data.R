1b4ab7e32fb8fae2cbe33ea02aff613526729885
Catarina Pien <catarina.pien@gmail.com>
2026-01-29 11:20:14 -0800

    reran locations to update column names

--- R/data.R
+++ R/data.R
@@ -2,5 +2,5 @@
 #' @title Aquarius Location Metadata
 #' @description Metadata for all locations in Aquarius database
 #' @format Data frame of 9 columns x Number of Aquarius locations
-#' @source Aquarius API, as called through the 'aq_get_location_list' function
+#' @source Aquarius API, as called through the `aq_get_location_list` function
 "aq_all_locations"
