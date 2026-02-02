64ac7495567492597ce71733bde504a7798f33f4
Catarina Pien <catarina.pien@gmail.com>
2026-02-02 10:53:17 -0800

    Reran data

--- R/data.R
+++ R/data.R
@@ -1,6 +1,8 @@
-#' Aquarius Location Metadata
+# Documentation of datasets
+
+#' Aquarius location metadata dataset
 #' @title Aquarius Location Metadata
-#' @description Metadata for all locations in Aquarius database
+#' @description Metadata dataset for all locations in Aquarius database. Updated when changes to locations occur.
 #' @format Data frame of 9 columns x Number of Aquarius locations
-#' @source Aquarius API, as called through the `aq_get_location_list` function
+#' @source Aquarius API, as called through the aq_get_location_list function
 "aq_all_locations"
