a77a7fbfb35d6f89e3e0dfa358fda3d8b5c77af9
Catarina Pien <catarina.pien@gmail.com>
2026-02-04 16:44:05 -0800

    Just removed some of the pre-written comments

--- data-raw/aq_all_locations.R
+++ data-raw/aq_all_locations.R
@@ -1,5 +1,5 @@
-## code to prepare `aq_all_locations` dataset goes here
 
+# Call aq_get_location_list to get list. Write as data object.
 aq_all_locations <- aq_get_location_list()
 usethis::use_data(aq_all_locations, overwrite=TRUE)
 
