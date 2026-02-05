c66c61731cfe8903bf48d850755aed056d4daeaf
Catarina Pien <catarina.pien@gmail.com>
2026-02-04 17:48:23 -0800

    Added functionality to get start and end dates of timeseries

--- R/aq_parameter_locations.R
+++ R/aq_parameter_locations.R
@@ -7,7 +7,7 @@
 #'
 #' @param param Parameter of interest
 
-#' @return A data frame containing columns for location information
+#' @return A data frame containing columns for location information and start and end times of timeseries
 #'
 #' @examples
 #' aq_get_parameter_locations("Water Temp")
@@ -15,12 +15,33 @@
 #' @export
 aq_get_parameter_locations <- function(param) {
 
+  # Check connection
+  aq_ensure_connection()
 
+  # Get time series descriptions
+  json_ts_des <- timeseries$getTimeSeriesDescriptions() |>
+    dplyr::select(aq_location_id = LocationIdentifier, parameter_name = Parameter, start_datetime = CorrectedStartTime,
+                  end_datetime = CorrectedEndTime, publish = Publish) |>
+    dplyr::right_join(deltawqAQ::aq_all_locations |>
+                        dplyr::select(aq_location_id, cdec_code, location_id, location_name), by = "aq_location_id") |>
+    dplyr::filter(!is.na(start_datetime),
+                  !is.na(end_datetime),
+                  parameter_name == param,
+                  publish == TRUE)
 
-  # Filter aq_all_parameter_locations data object to the parameter selected
-  params_filtered <- deltawqAQ::aq_all_parameter_locations %>%
-    dplyr::filter(parameter_name == param) %>%
-    dplyr::select(parameter_name, cdec_code, location_id, aq_location_name, aq_location_id)
+  # Filter aq_all_parameter_locations data object to the parameter selected and add additional details
+  params_filtered <- deltawqAQ::aq_all_parameters |>
+    dplyr::filter(parameter_name == param) |>
+    # add start and end date times
+    dplyr::left_join(json_ts_des) |>
+    # final selection of display columns
+    dplyr::select(parameter_name,
+                  cdec_code,
+                  location_id,
+                  location_name,
+                  aq_location_id,
+                  start_datetime,
+                  end_datetime)
 
   # Return df
   return(params_filtered)
