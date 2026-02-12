0f3898c64f99589df7d3a340ff70fe89b79180c3
Catarina Pien <catarina.pien@gmail.com>
2026-02-11 17:07:14 -0800

    reran data objects to update latest information and naming

--- data-raw/aq_all_parameters.R
+++ data-raw/aq_all_parameters.R
@@ -1,5 +1,5 @@
-aq_all_parameters <- deltawqAQ::aq_all_parameter_locations %>%
-  select(parameter_name, unit) %>%
-  distinct()
+aq_all_parameters <- deltawqAQ::aq_parameter_location_crosswalk |>
+  dplyr::select(parameter_name, unit) |>
+  dplyr::distinct()
 
 usethis::use_data(aq_all_parameters, overwrite = TRUE)
